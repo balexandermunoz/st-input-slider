f199ad77978903afff868def1efafdfee8de9eac
Brayan Munoz <balexander.munoz@udea.edu.co>
2024-04-08 12:36:42 -0500

    :memo: Update example

--- st_input_slider/example.py
+++ st_input_slider/example.py
@@ -1,10 +1,13 @@
 import streamlit as st
+
 from st_input_slider import st_input_slider
 
 st.title("Slider + Input Example!")
-options = {"color": "#9E9A35",
-           "inputWidth": "58px",
-           "disableUnderline": True}
+options = {
+    "color": "#9E9A35",
+    "inputWidth": "58px",
+    "disableUnderline": True
+}
 num_clicks = st_input_slider(
     label="A custom slider", min_value=-100, max_value=200, value=50, step=0.1, options=options)
 st.markdown("Value: %s" % (num_clicks))
