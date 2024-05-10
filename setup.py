830dce25c074391ae8b48a7d6e041287d73b89db
Brayan Munoz <balexander.munoz@udea.edu.co>
2024-05-10 15:47:31 -0500

    :deploy: Relase version 0.1.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="st-input-slider",
-    version="0.1.1",
+    version="0.1.2",
     author="Brayan Munoz",
     author_email="balexander.munoz@udea.edu.co",
     description="A custom Streamlit component that combines a slider with an input box for more precise value selection",
